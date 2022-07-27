f56401a54cdfc9f93e37c2e5a014f9d033c56333
MahmoudSumairat <roys.msc2017@gmail.com>
2022-07-27 16:50:09 +0300

    initialzed the font family and the color variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-     Hello and welcome to Plus FIT <strong>Boss, Mahmoud!</strong>
+      Hello and welcome to Plus FIT <strong>Boss, Mahmoud!</strong>
     </div>
   );
 }
