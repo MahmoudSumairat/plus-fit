f4c979fadc38719257da34f4feb489a4d42c461e
MahmoudSumairat <roys.msc2017@gmail.com>
2022-08-12 16:59:00 +0300

    added api initialization

---
+++ src/API/baseURLs.js
@@ -0,0 +1,6 @@
+export const PRODUCTS = "/products";
+export const BAG = "/bag";
+export const LOOKUP = "/lookup";
+export const CHECKOUT = "/checkout";
+export const AUTH = "/auth";
+export const LANDING_PAGE = "/landing-page";
--- src/http/http.js
+++ src/http/http.js
@@ -4,13 +4,13 @@ import authInterceptor from "./interceptors/authInterceptor";
 import requestError from "./interceptors/requestError";
 
 const http = axios.create({
-baseURL : BASE_URL,
-headers : {
-    "Content-Type" : 'application/json'
-}
+  baseURL: BASE_URL,
+  timeout: 1000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 http.interceptors.request.use(authInterceptor, requestError);
-
 
 export default http;
