9b16883cc3bf050471b835fa5c4b1e68df57ca1e
Mahmoud Bassam <mahmoudbassam@dev-malsmairat.home>
2022-09-17 23:31:28 +0300

    added deployment stuff

--- src/config/baseURL.js
+++ src/config/baseURL.js
@@ -1,1 +1,5 @@
-export const BASE_URL = process.env.REACT_APP_API_URL;
+const isProd = process.env.NODE_ENV === "prod";
+
+export const BASE_URL = isProd
+  ? process.env.REACT_APP_LIVE_API_URL
+  : process.env.REACT_APP_LOCAL_API_URL;
--- src/http/http.js
+++ src/http/http.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "../config/baseURL";
 import authInterceptor from "./interceptors/authInterceptor";
 import requestError from "./interceptors/requestError";
-
+console.log(BASE_URL);
 const http = axios.create({
   baseURL: BASE_URL,
   timeout: 5000,
